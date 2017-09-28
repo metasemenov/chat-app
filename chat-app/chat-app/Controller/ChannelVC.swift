a8156eea900c91e85cc517ecda6457351224e55d
Admin <admin@Mac-Admin.local>
2017-09-28 17:47:33 +0300

    added ui for channels

---
+++ chat-app/chat-app/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  chat-app
+//
+//  Created by Admin on 28.09.17.
+//  Copyright © 2017 metasemenov. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+
+    }
+
+
+}
